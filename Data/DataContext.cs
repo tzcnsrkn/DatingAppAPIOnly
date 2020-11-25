bf8b5b464d400b9675b9b7ec115bfcc53facbe83
Stezcan <STezcan@lely.com>
2020-11-25 14:21:16 +0300

    --deneme4

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,12 +6,13 @@ namespace DatingApp.API.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext>  options) : base (options) {}
-        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //     => optionsBuilder.UseNpgsql(
-        //         @"Host=ec2-52-206-44-27.compute-1.amazonaws.com
-        //         Database=dc65dm8pteg7v2;
-        //         Username=ximhpeyexhvcda;
-        //         Password=6fb4c86d18c96fff31e0a347321544191849f80c11724e6a4993e2e9083a986e");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            // => optionsBuilder.UseNpgsql("Host=localhost;Database=DatingApp;Username=postgres;Password=321lely");
+            => optionsBuilder.UseNpgsql(
+                @"Host=ec2-52-206-44-27.compute-1.amazonaws.com
+                Database=dc65dm8pteg7v2;
+                Username=ximhpeyexhvcda;
+                Password=6fb4c86d18c96fff31e0a347321544191849f80c11724e6a4993e2e9083a986e");        
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }      
         public DbSet<Photo> Photos { get; set; }  
--- Startup.cs
+++ Startup.cs
@@ -38,11 +38,13 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             // services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            // services.AddDbContext<DataContext>(x => x.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<DataContext>(x => x.UseNpgsql(
                 @"Host=ec2-52-206-44-27.compute-1.amazonaws.com
                 Database=dc65dm8pteg7v2;
                 Username=ximhpeyexhvcda;
                 Password=6fb4c86d18c96fff31e0a347321544191849f80c11724e6a4993e2e9083a986e"));
+                            
             services.AddControllers().AddNewtonsoftJson(opt => {
                     opt.SerializerSettings.ReferenceLoopHandling = 
                         Newtonsoft.Json.ReferenceLoopHandling.Ignore;
